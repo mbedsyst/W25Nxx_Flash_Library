369967cc625fcd5fb7fc87a1b7f3745fb350c222
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2024-12-01 23:22:28 +0530

    Updated Device specific macros

--- Inc/W25Nxx.h
+++ Inc/W25Nxx.h
@@ -3,6 +3,16 @@
 
 #include "SPI.h"
 
+// W25N01GV Device Macros
+#define W25N_PAGE_SIZE		2048
+#define W25N_PAGE_COUNT		65536
+#define W25N_BLOCK_SIZE		64
+#define W25N_BLOCK_COUNT	1024
+#define W25N_OTP_SIZE		2048
+#define W25N_OTP_COUNT		10
+#define W25N_TOTAL_SIZE		134217728
+
+
 void W25N_Reset(void);
 uint32_t W25N_ReadJEDEC_ID(void);
 void W25N_WriteEnable(void);
