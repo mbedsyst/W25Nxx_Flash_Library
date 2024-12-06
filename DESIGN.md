7dcec6f1cd63211fe7b54ad4938e7085401075dc
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2024-12-06 22:04:47 +0530

    Designed Status Reg Parsing and Setting functions

--- DESIGN.md
+++ DESIGN.md
@@ -1,18 +1,18 @@
 # FLASH Memory Instructions List
 
-/ Device Reset____________________|FFh
-/ JEDEC ID________________________|9Fh Dummy EFh AAh 21h
-/ Read Status Register____________|0Fh/05h SR Addr S7-0 S7-0 S7-0 S7-0 S7-0 S7-0 S7-0
-/ Write Status Register___________|1Fh/01h SR Addr S7-0
-/ Write Enable____________________|06h
-/ Write Disable___________________|04h
-* BB Management___________________|A1h LBA LBA PBA PBA
-* Read BBM LUT____________________|A5h Dummy LBA0 LBA0 PBA0 PBA0 LBA1 LBA1 PBA1
-* Last ECC failure________________|A9h Dummy PA15-8 PA7-0 				
-/ Block Erase_____________________|D8h Dummy PA15-8 PA7-0
-/ Program Data Load_______________|02h CA15-8 CA7-0 Data-0 Data-1 Data-2 Data-3 Data-4 Data-5
-* Random Program__________________|84h CA15-8 CA7-0 Data-0 Data-1 Data-2 Data-3 Data-4 Data-5
-/ Program Execute_________________|10h Dummy PA15-8 PA7-0
-/ Page Data Read__________________|13h Dummy PA15-8 PA7-0
-/ Read____________________________|03h Dummy Dummy Dummy D7-0 D7-0 D7-0 D7-0 D7-0
-* Fast Read_______________________|0Bh Dummy Dummy Dummy Dummy D7-0 D7-0 D7-0 D7-0
+/ Device Reset___________|FFh
+/ JEDEC ID_______________|9Fh Dummy EFh AAh 21h
+/ Read Status Register___|0Fh/05h SR Addr S7-0 S7-0 S7-0 S7-0 S7-0 S7-0 S7-0
+/ Write Status Register__|1Fh/01h SR Addr S7-0
+/ Write Enable___________|06h
+/ Write Disable__________|04h
+* BB Management__________|A1h LBA LBA PBA PBA
+* Read BBM LUT___________|A5h Dummy LBA0 LBA0 PBA0 PBA0 LBA1 LBA1 PBA1
+* Last ECC failure_______|A9h Dummy PA15-8 PA7-0 				
+/ Block Erase____________|D8h Dummy PA15-8 PA7-0
+/ Program Data Load______|02h CA15-8 CA7-0 Data-0 Data-1 Data-2 Data-3 Data-4 Data-5
+* Random Program_________|84h CA15-8 CA7-0 Data-0 Data-1 Data-2 Data-3 Data-4 Data-5
+/ Program Execute________|10h Dummy PA15-8 PA7-0
+/ Page Data Read_________|13h Dummy PA15-8 PA7-0
+/ Read___________________|03h Dummy Dummy Dummy D7-0 D7-0 D7-0 D7-0 D7-0
+* Fast Read______________|0Bh Dummy Dummy Dummy Dummy D7-0 D7-0 D7-0 D7-0
--- Src/W25Nxx.c
+++ Src/W25Nxx.c
@@ -23,6 +23,30 @@
 #define STATUS_REG2			  0xB0
 #define STATUS_REG3			  0xC0
 
+// Status Register-1 Bit Positions
+#define W25N_SR1_BP0        (1 << 3)  // Block Protect Bit 0
+#define W25N_SR1_BP1        (1 << 4)  // Block Protect Bit 1
+#define W25N_SR1_BP2        (1 << 5)  // Block Protect Bit 2
+#define W25N_SR1_BP3        (1 << 6)  // Block Protect Bit 3
+#define W25N_SR1_TB         (1 << 2)  // Top/Bottom Block Protect
+#define W25N_SR1_WP_E       (1 << 1)  // Write Protection Enable
+
+// Status Register-2 Bit Positions
+#define W25N_SR2_BUF        (1 << 3)  // Buffer Read Mode
+#define W25N_SR2_ECC_E      (1 << 4)  // ECC Enable
+#define W25N_SR2_SR1_L      (1 << 6)  // Status Register-1 Lock
+#define W25N_SR2_OTP_E      (1 << 2)  // OTP Access Mode Enable
+
+// Status Register-3 Bit Positions
+#define W25N_SR3_LUT_F      (1 << 0)  // Look-Up Table Full
+#define W25N_SR3_ECC_0      (1 << 2)  // ECC Status Bit 0
+#define W25N_SR3_ECC_1      (1 << 3)  // ECC Status Bit 1
+#define W25N_SR3_P_FAIL     (1 << 4)  // Program Failure
+#define W25N_SR3_E_FAIL     (1 << 5)  // Erase Failure
+#define W25N_SR3_WEL        (1 << 1)  // Write Enable Latch
+#define W25N_SR3_BUSY       (1 << 0)  // Erase/Program In Progress
+
+
 static void W25N_WriteEnable(void)
 {
 	SPI2_SelectSlave();
@@ -74,7 +98,7 @@ uint8_t W25N_ReadStatusReg(uint8_t addr)
 	return status_reg;
 }
 
-void W25N_WriteStatusReg(uint8_t addr, uint8_t data)
+static void W25N_WriteStatusReg(uint8_t addr, uint8_t data)
 {
 	SPI2_SelectSlave();
 	SPI2_TransmitReceiveByte(WRITE_STATUS_REG);
@@ -83,7 +107,7 @@ void W25N_WriteStatusReg(uint8_t addr, uint8_t data)
 	SPI2_DeselectSlave();
 }
 
-void W25N_EraseBlock(uint16_t blockNumber)
+static void W25N_EraseBlock(uint16_t blockNumber)
 {
 	uint16_t addr = (blockNumber * 64 * 2048); // 1 Block contains 64 Pages each of 2048 bytes (131072 bytes)
 	W25N_WriteEnable();
@@ -94,6 +118,78 @@ void W25N_EraseBlock(uint16_t blockNumber)
 	SPI2_TransmitReceiveByte((addr) & 0xFF);
 	SPI2_DeselectSlave();
 	delay_ms(10);
+}
+
+uint8_t W25N_GetStatusBit(uint8_t reg_addr, uint8_t bit_pos)
+{
+    uint8_t reg_value = W25N_ReadStatusReg(reg_addr);
+    return (reg_value >> bit_pos) & 0x01;
+}
+
+void W25N_SetStatusBit(uint8_t reg_addr, uint8_t bit_pos, uint8_t value)
+{
+    uint8_t reg_value = W25N_ReadStatusReg(reg_addr);
+
+    if (value)	reg_value |= (1 << bit_pos);
+    else        reg_value &= ~(1 << bit_pos);
+
+    W25N_WriteStatusReg(reg_addr, reg_value);
+}
+
+uint8_t W25N_GetBlockProtectBits()
+{
+    uint8_t reg_value = W25N_ReadStatusReg(STATUS_REG1);
+    return (reg_value >> 2) & 0x1F;
+}
+
+void W25N_SetBlockProtectBits(uint8_t bp_bits)
+{
+    uint8_t reg_value = W25N_ReadStatusReg(STATUS_REG1);
+    reg_value = (reg_value & 0xC3) | ((bp_bits & 0x1F) << 2); // Update BP3-BP0, TB bits
+    W25N_WriteStatusReg(STATUS_REG1, reg_value);
+}
+
+uint8_t W25N_GetEccEnable()
+{
+    return W25N_GetStatusBit(STATUS_REG2, 4);
+}
+
+void W25N_SetEccEnable(uint8_t enable)
+{
+    W25N_SetStatusBit(STATUS_REG2, 4, enable);
+}
+
+uint8_t W25N_GetBufferReadMode()
+{
+    return W25N_GetStatusBit(STATUS_REG2, 3);
+}
+
+void W25N_SetBufferReadMode(uint8_t mode)
+{
+    W25N_SetStatusBit(STATUS_REG2, 3, mode);
+}
+
+uint8_t W25N_GetWriteProtectionEnable()
+{
+    return W25N_GetStatusBit(STATUS_REG1, 1);
+}
+
+void W25N_SetWriteProtectionEnable(uint8_t enable)
+{
+    W25N_SetStatusBit(STATUS_REG1, 1, enable);
+}
+
+uint8_t W25N_GetStatusRegisterProtectBits()
+{
+    uint8_t reg_value = W25N_ReadStatusReg(STATUS_REG1);
+    return (reg_value & 0x81);
+}
+
+void W25N_SetStatusRegisterProtectBits(uint8_t srp_bits)
+{
+    uint8_t reg_value = W25N_ReadStatusReg(STATUS_REG1);
+    reg_value = (reg_value & 0x7E) | (srp_bits & 0x81);
+    W25N_WriteStatusReg(STATUS_REG1, reg_value);
 }
 
 static void W25N_ExecuteWrite(uint16_t addr)
@@ -108,11 +204,11 @@ static void W25N_ExecuteWrite(uint16_t addr)
 
 void W25N_WritePage(uint16_t pageNumber, uint8_t *data, uint16_t size)
 {
-	uint16_t addr = (pageNumber * 2048);
+	uint16_t addr = (pageNumber * 2112);
 	W25N_WriteEnable();
 	SPI2_SelectSlave();
 	SPI2_TransmitReceiveByte(PROG_DATA_LOAD);
-	SPI2_TransmitReceiveByte((addr >> 8) & 0x0F);
+	SPI2_TransmitReceiveByte((addr >> 8) & 0xFF);
 	SPI2_TransmitReceiveByte((addr) & 0xFF);
 	SPI2_TransmitReceive_MultiByte(data, NULL, size);
 	SPI2_DeselectSlave();
@@ -134,7 +230,7 @@ static void W25N_ExecuteRead(uint16_t addr, uint8_t *data, uint16_t size)
 
 void W25N_ReadPage(uint16_t pageNumber, uint8_t *data, uint16_t size)
 {
-	uint16_t addr = (pageNumber * 2048);
+	uint16_t addr = (pageNumber * 2112);
 	SPI2_SelectSlave();
 	SPI2_TransmitReceiveByte(PAGE_READ);
 	SPI2_TransmitReceiveByte(0xFF);
@@ -144,3 +240,17 @@ void W25N_ReadPage(uint16_t pageNumber, uint8_t *data, uint16_t size)
 	delay_ms(1);
 	W25N_ExecuteRead(addr, data, size);
 }
+
+void W25N_FastReadPage(uint16_t pageNumber, uint8_t *data, uint16_t size)
+{
+	uint16_t addr = (pageNumber * 2112);
+	SPI2_SelectSlave();
+	SPI2_TransmitReceiveByte(FAST_READ);
+	SPI2_TransmitReceiveByte((addr >> 8) & 0xFF);
+	SPI2_TransmitReceiveByte((addr) & 0xFF);
+	SPI2_TransmitReceiveByte(0xFF);
+	SPI2_DeselectSlave();
+	delay_ms(1);
+	W25N_ExecuteRead(addr, data, size);
+}
+
